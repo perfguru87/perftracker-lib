f53981e7da7471f5f8164eaaeb3de6f6e595ebe5
perfguru87 <perfguru87@gmail.com>
2018-04-17 02:17:10 +0300

    perfrackerlib - added browser lib

---
+++ perftrackerlib/browser/browser_firefox.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+# -*- coding: utf-8 -*-
+__author__ = "perfguru87@gmail.com"
+__copyright__ = "Copyright 2018, The PerfTracker project"
+__license__ = "MIT"
+
+"""
+The Firefox browser helper
+"""
+
+import logging
+import httputils
+import re
+import os
+import datetime
+import time
+import json
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from helpers.timeparser import TimeParser
+from helpers.largelogfile import LargeLogFile
+import helpers.timeutil as timeutil
+
+from browser.utils import parse_url, get_val
+from browser.browser_webdriver import BrowserWebdriver, abort
+from browser.browser_base import BrowserExc
+from browser.page import PageRequest, PageTimeline
+
+try:
+    from selenium import webdriver
+    from selenium.common.exceptions import WebDriverException
+    import psutil
+except ImportError as e:
+    abort(e)
+
+
+# reFF = re.compile("(\d+-\d+-\d+ \d+:\d+\:\d+.\d+) UTC - [-\d]+\[([\d\w]+)\]:\s+(.*)\n")
+reFF = re.compile(" UTC - [-\d]+\[([\d\w]+)\]:\s+(.*)\n")
+reFFTxnInit = re.compile("^nsHttpTransaction::Init \[this=([\d\w]+).*$")
+reFFProcessData = re.compile("^nsHttpTransaction::ProcessData \[this=([\d\w]+) count=(\d+)\].*$")
+reFFReadFromcache = re.compile("^nsHttpChannel::ReadFromCache \[this=([\d\w]+)] Using cached copy of: (.*)$")
+reFFDestroyTxn = re.compile("^Destroying nsHttpTransaction @([\d\w]+).*$")
+
+
+class BrowserFirefox(BrowserWebdriver):
+    engine = "firefox"
+    skip_urls = ["cdn.mozilla.net", "mozilla.org", "mozilla.com", "ocsp.digicert.com", "openh264.org"]
+
+    def __init__(self, *args, **kwargs):
+        BrowserWebdriver.__init__(self, *args, **kwargs)
+        self._last_seen_dt = datetime.datetime.utcnow()
+
+    def _skip_url(self, page, url):
+        if url and url.startswith("about:"):
+            return True
+
+        return BrowserWebdriver._skip_url(self, page, url)
+
+    def _browser_get_rss_kb(self):
+        return psutil.Process(self.pid).memory_info().rss / 1024
+
+    def browser_start(self):
+        os.environ['NSPR_LOG_FILE'] = self.log_path
+        os.environ['NSPR_LOG_MODULES'] = 'timestamp,nsHttp:5,nsSocketTransport:5,nsStreamPump:5,nsHostResolver:5'
+
+        profile = webdriver.FirefoxProfile()
+        profile.accept_untrusted_certs = True
+
+        # sometimes (bug? race?) firefox fails to start wihout clear reason, so lets try to restart it several times
+        retry = 5
+        while True:
+            try:
+                self.driver = webdriver.Firefox(firefox_profile=profile)
+            except WebDriverException as e:
+                if retry:
+                    self.log_warning("firefox start failed, retrying...")
+                    time.sleep(1)
+                    retry -= 1
+                    continue
+                self.log_error("Firefox browser start failed, see browser logs here: %s" % self.log_path)
+                if "Can't load the profile" in str(e):
+                    # http://stackoverflow.com/questions/6682009/selenium-firefoxprofile-exception-cant-load-the-profile
+                    self.log_error("Try to update Firefox and selenium: pip install -U selenium")
+                if "geckodriver" in str(e):
+                    self.log_error("Try to install geckodriver from https://github.com/mozilla/geckodriver/releases and"
+                                   " put it to PATH")
+                raise BrowserExc(str(e))
+            break
+
+        # old native firefox driver
+        if self.driver.binary:
+            return self.driver.binary.process.pid
+
+        # geckodriver
+        return self.driver.service.process.pid
+
+    def _browser_get_events(self, page):
+        thread2req = {}
+        thread2resp = {}
+        thread2txn = {}
+        path2prot = {}
+
+        tp = TimeParser()
+
+        start = self._last_seen_dt
+
+        self.log_debug("Reading %s starting from %s" % (self.log_path, start))
+
+        log = LargeLogFile(self.log_path,
+                           start.strftime("%Y-%m-%d %H:%M:%S.000000"),
+                           "2099-01-01 00:00:00.000000")
+
+        for dt, line in log.readlines_with_time():
+
+            if logging.getLogger().getEffectiveLevel() == logging.DEBUG:
+                self.log_debug("LOG: %s" % line.strip())
+
+            m = reFF.match(line)
+            if not m:
+                continue
+
+            self._last_seen_dt = dt
+
+            ts = timeutil.datetime_utc_to_timestamp(dt) * 1000
+            thread = m.group(2)
+            msg = m.group(3)
+
+            m = reFFTxnInit.match(msg)
+            if m:
+                thread2txn[thread] = m.group(1)
+                continue
+
+            m = reFFProcessData.match(msg)
+            if m:
+                txn = m.group(1)
+                thread2txn[thread] = txn
+                req = page.get_request(txn)
+                if req:
+                    req.length += int(m.group(2))
+                continue
+
+            if msg.startswith("uri="):
+                url = msg[4:]
+                prot, domain, path = parse_url(url)
+                path2prot["%s%s" % (domain, path)] = prot
+
+            if msg.startswith("nsHttpChannel::ReadFromCache"):
+                m = reFFReadFromcache.match(msg)
+                if m:
+                    txn = m.group(1)
+                    r = PageRequest(page, txn)
+                    r.method = "GET"
+                    r.url = m.group(2)
+                    r.status = 200
+                    r.start(ts)
+                    r.complete(ts)
+                    r.cached = True
+                    page.process_activity("read request from cache", ts)
+                    page.add_request(r)
+
+            m = reFFDestroyTxn.match(msg)
+            if m:
+                txn = m.group(1)
+                r = page.get_request(txn)
+                if r and not r.completed:
+                    self.log_debug("WARNING: request '%s' has been completed abnormally" % r.id)
+                    r.complete(ts)
+                continue
+
+            if thread not in thread2txn:
+                continue
+
+            txn = thread2txn[thread]
+
+            if msg.startswith("http request"):
+                thread2req[thread] = []
+                page.add_request(PageRequest(page, txn))
+                continue
+
+            if thread in thread2req:
+                if msg == "]":
+                    req = page.get_request(txn)
+                    r = httputils.HTTPRequestFromStr("\n".join(thread2req[thread]))
+                    req.method = r.command
+
+                    p = "%s%s" % (r.headers['host'], r.path)
+                    _, domain, path = parse_url("http://" + p)
+                    path = "%s%s" % (domain, path)
+
+                    skip = False
+
+                    if self._skip_url(page, path):
+                        skip = True
+                        self.log_debug("ignore/skip request to: %s" % path)
+                    elif path in path2prot:
+                        req.url = "%s://%s" % (path2prot[path], p)
+                    else:
+                        self.log_warning("Can't determine protocol for url: %s, skipping!" % p)
+                        skip = True
+
+                    if skip:
+                        del thread2req[thread]
+                        page.del_request(req)
+                        continue
+
+                    req.header = r.headers.dict
+
+                    del thread2req[thread]
+                    page.process_activity("request", ts)
+                    req.start(ts)
+                else:
+                    thread2req[thread].append(msg)
+
+            if msg.startswith("http response"):
+                if page.get_request(txn):
+                    thread2resp[thread] = []
+                    continue
+
+            if thread in thread2resp:
+                if msg == "]":
+                    req = page.get_request(txn)
+                    if not req:
+                        continue
+                    r = httputils.HTTPResponseFromStr("\n".join(thread2resp[thread]))
+                    # print("\n".join(thread2resp[thread]))
+                    len = r.getheader('Content-Length')
+                    if len:
+                        req.content_length = int(len)
+                    req.status = r.status
+                    type = r.getheader("Content-Type")
+                    if type:
+                        req.set_type(type)
+                    req.keepalive = r.getheader("Connection") in ("Keep-Alive", "keep-alive")
+                    req.gzipped = r.getheader("Content-Encoding") == "gzip"
+
+                    del thread2resp[thread]
+                    page.process_activity("response", ts)
+                    req.complete(ts)
+                else:
+                    thread2resp[thread].append(msg)
+
+
+##############################################################################
+# Autotests
+##############################################################################
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    try:
+        b = BrowserFirefox()
+    except BrowserExc as e:
+        sys.exit(-1)
+    b.navigate_to("https://example.com/")
+    b.browser_stop()
---
+++ perftrackerlib/browser/browser_python.py
@@ -0,0 +1,376 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+# -*- coding: utf-8 -*-
+__author__ = "perfguru87@gmail.com"
+__copyright__ = "Copyright 2018, The PerfTracker project"
+__license__ = "MIT"
+
+"""
+Naive Python browser, it is able to repeat requests traces collected from a real browser
+"""
+
+import os
+import sys
+import logging
+import copy
+import socket
+import re
+import time
+
+if sys.version_info[0] < 3:
+    from Cookie import SimpleCookie
+else:
+    from http.cookies import SimpleCookie
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from browser.browser_base import BrowserBase, DEFAULT_WAIT_TIMEOUT, BrowserExc
+from browser.page import Page, PageRequest, PageRequestsGroup, PageWithActions
+from browser.utils import parse_url, extract_cookies
+from helpers.httppool import HTTPPool
+
+from threading import Lock
+from multiprocessing.dummy import Pool as ThreadPool
+import httputils
+
+
+class BrowserPythonNetlocData:
+    def __init__(self, browser, netloc):
+
+        # Remove standard port number from netloc string
+        # Allowed by HTTP W3C and make some web-apps happy
+        if ':' in netloc:
+            nl = netloc.split(':')
+            if nl[1] == '80' or nl[1] == '443':
+                netloc = nl[0]
+
+        self.browser = browser
+        self.netloc = netloc
+
+        self.cookies = SimpleCookie()
+        self.httpool = {}
+        self.lock = Lock()
+
+        self.header = {}
+        self.header['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0 PYTHON'
+        self.header['Connection'] = 'keep-alive'
+        self.header['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+        self.header['Accept-Language'] = 'en-US,en;q=0.5'
+        self.header['Content-Type'] = 'application/x-www-form-urlencoded'
+        self.header['Host'] = self.netloc
+
+    def _get_http_pool(self, scheme):
+        if scheme not in self.httpool:
+            loc = "%s://%s" % (scheme, self.netloc)
+            self.browser.log_debug("allocating connection pool to %s" % loc)
+            self.httpool[scheme] = HTTPPool("%s" % loc)
+        return self.httpool[scheme]
+
+    def __del__(self):
+        for pool in self.httpool.values():
+            pool.clear()
+
+    def set_cookie(self, url, key, val=None, path=None):
+        self.browser.log_debug("set cookie: %s, %s=%s" % (url, key, str(val)))
+
+        scheme, netloc, path = parse_url(url)
+        with self.lock:
+            if type(key) == 'dict':
+                c = key
+                self.cookies[c['name']] = c
+            elif val:
+                self.cookies[key] = val
+                if path:
+                    self.cookies[key]["path"] = path
+            elif path in self.cookies:
+                del self.cookies[key]
+
+    def set_header(self, key, val):
+        self.browser.log_debug("set header: %s=%s" % (key, val))
+        with self.lock:
+            if val is not None:
+                self.header[key] = val
+            elif key in self.header:
+                del self.header[key]
+
+    def get_cookies_str(self, url):
+        """
+        get_cookies_str() returns only the cookies applicable to given *url*
+        """
+
+        _, _, path = parse_url(url)
+
+        path_cookies = {}
+        for key, morsel in self.cookies.items():
+            p = morsel["path"] if morsel["path"] else "/"
+            if key not in path_cookies:
+                path_cookies[key] = {}
+            path_cookies[key][p] = morsel.value
+
+        cookies = {}
+
+        for key, paths in path_cookies.items():
+            # sort by string length: ["/home/path", "/home", "/"]
+            for p in sorted(paths.keys(), lambda x: len(x), reverse=True):
+                if path.startswith(p):
+                    cookies[key] = paths[p]
+                    break
+
+        return "; ".join("%s=%s" % (key, val) for key, val in cookies.items())
+
+    def _get_redirected_request(self, req, response):
+        new_req = None
+        if req.status in [301, 302, 303]:
+            new_req = req.duplicate()
+            new_req.method = 'GET'
+            new_req.params = None
+            new_req.url = response.getheader('location')
+        elif self.browser.js_redirects:
+            page_actions = httputils.PageActionsExtractor(req.data)
+            redirect = page_actions.get_action()
+            if redirect:
+                new_req = req.duplicate()
+                new_req.method = redirect.method
+                new_req.params = redirect.get_params()
+                new_req.url = redirect.url
+                new_req.header['Cookie'] = self.browser.browser_get_cookies_str(req.url)
+            req.page_actions = page_actions
+
+        if new_req:
+            new_req.validator = None
+            new_req.header['Cookie'] = self.browser.browser_get_cookies_str(req.url)
+            if not new_req.url.startswith('http'):
+                new_req.update_netloc(req.url)
+            _, new_req.header['Host'], _ = parse_url(new_req.url)
+            return new_req
+
+        return None
+
+    def _execute_page_request(self, pool, page, req, path_with_args):
+        assert(req.method == 'POST' or not req.params)  # only POST request may have params
+
+        with pool.borrow() as (conn, is_new):
+            # conn.set_debuglevel(1)
+            conn.request(req.method, path_with_args, req.params, req.header)
+            self.header['Referer'] = req.url
+            response = conn.getresponse()
+            req.status = response.status
+            req.data = response.read()
+
+            self.browser.log_debug(" req %s HTTP response: %s, headers: %s" %
+                                   (req.id, req.status, response.getheaders()))
+
+            # handle new cookies
+            for cookie in extract_cookies(response):
+                if sys.version_info[0] < 3 and isinstance(cookie, unicode):
+                    cookie = cookie.encode('ascii', 'ignore')
+                self.cookies.load(cookie)
+
+            # handle redirection
+            new_req = self._get_redirected_request(req, response)
+            if new_req:
+                self.browser.log_debug(" req %s, redirect to %s" % (new_req.id, new_req.url))
+                page.add_request(new_req)
+
+                # FIXME:
+                # 1. _get_netloc_data is private method of the browser.
+
+                self.browser._get_netloc_data(new_req.url).execute_page_request(page, new_req)
+                req.status = new_req.status
+                req.data = new_req.data
+
+            if req.valid_statuses and req.status not in req.valid_statuses:
+                raise BrowserExc(' req %s, %s %s status %d'
+                                 (req.id, req.method, req.url, req.status))
+
+        if self.browser.validation:
+            req.validate_response(req.data)
+
+        req.complete()
+        return
+
+    def execute_page_request(self, page, req):
+
+        scheme, netloc, path_with_args = parse_url(req.url, args=True)
+
+        if scheme not in ("http", "https", "ftp"):
+            msg = "Can't execute request on URL with unsupported scheme: %s, %s" % (scheme, url)
+            self.browser.log_error(msg)
+            raise BrowserExc(msg)
+
+        req.start()
+
+        pool = self._get_http_pool(scheme)
+        # 10 - is technological retry count to handle closed keep-alive connections
+        for loop in range(10):
+            try:
+                self._execute_page_request(pool, page, req, path_with_args)
+                return
+
+            except pool.temporary_errors as ex:
+                raise  # aandreev
+                # usually it means server has closed keep-alive connection due to timeout. lets retry
+                self.browser.log_debug("HTTP Exception: %s %s: %s %s, connection closed? retrying" %
+                                       (req.method, req.url, type(ex), str(ex)))
+                req.status = str(type(ex))
+                time.sleep(0.1 * loop)
+
+            except pool.fatal_errors as ex:
+                req.status = str(type(ex))
+                req.complete()
+                self.browser.log_error("HTTP Exception: %s %s: %s %s" % (req.method, req.url, type(ex), str(ex)))
+                return
+
+        req.complete()
+        self.browser.log_error("HTTPException: %s %s, all retries failed" % (req.method, req.url))
+
+    def execute_page_request_parallel(self, page, reqs, parallel=8):
+        if not len(reqs):
+            return
+        if len(reqs) == 1:
+            self.execute_page_request(page, reqs[0])
+            return
+
+        def execute(arg):
+            global data
+            page, req = arg
+            try:
+                self.execute_page_request(page, req)
+            except RuntimeError:
+                import traceback
+                self.browser.log_error("traceback:\n" + traceback.format_exc())
+            return 0
+
+        args = [(page, req) for req in reqs]
+
+        if parallel > len(reqs):
+            parallel = len(reqs)
+
+        pool = ThreadPool(parallel)
+        pool.map(execute, args)
+        pool.close()
+        pool.join()
+
+
+class BrowserPython(BrowserBase):
+    engine = "pybrowser"
+
+    def __init__(self, headless=True, validation=True, cleanup=True, max_connections=8,
+                 js_redirects=False, log_path=None):
+        BrowserBase.__init__(self, cleanup=cleanup, log_path=log_path)
+
+        self.validation = validation
+        self.max_connections = max_connections
+        self.js_redirects = js_redirects  # try to parse page to detect JS and other ways of redirect
+
+        self._netloc_data = {}
+
+    def _get_netloc_data(self, url):
+        _, netloc, _ = parse_url(url)
+        if netloc not in self._netloc_data:
+            self._netloc_data[netloc] = BrowserPythonNetlocData(self, netloc)
+        return self._netloc_data[netloc]
+
+    def _browser_navigate(self, location, cached=True, name=None):
+        if isinstance(location, Page):
+            page = copy.deepcopy(location)
+        else:
+            return self._http_request("GET", location)
+
+        if not len(page.requests_groups):
+            raise BrowserExc("BrowserPython._browser_navigate() - bug: requests group is empty!")
+
+        page.browser = self
+        page.start()
+
+        for g in page.requests_groups:
+            reqs = g.get_uncached_reqs()
+            if not reqs:
+                continue
+            self._get_netloc_data(page.url).execute_page_request_parallel(page, reqs, parallel=self.max_connections)
+
+        page.complete(self)
+        return page
+
+    def _browser_wait(self, page, timeout=DEFAULT_WAIT_TIMEOUT):
+        return
+
+    def _browser_warmup_page(self, location, name=None):
+        return
+
+    def browser_get_name(self):
+        return "Python HTTP/1.x browser"
+
+    def browser_set_cookie(self, url, key, val=None, path=None):
+        self._get_netloc_data(url).set_cookie(url, key, val, path)
+
+    def browser_get_cookies_str(self, url):
+        return self._get_netloc_data(url).get_cookies_str(url)
+
+    def browser_set_header(self, url, key, val):
+        self._get_netloc_data(url).set_header(key, val)
+
+    def browser_start(self):
+        return os.getpid()
+
+    def browser_stop(self):
+        pass
+
+    def browser_reset(self):
+        self._netloc_data.clear()
+
+    # === BrowserPython specific === #
+
+    def _http_request(self, method, url, params=None, validator=None, header=None, valid_statuses=None):
+        page = Page(self, url)
+        req = PageRequest(page)
+
+        req.header.update(self._get_netloc_data(url).header)
+        req.header['Cookie'] = self._get_netloc_data(url).get_cookies_str(url)
+        if header:
+            req.header.update(header)
+
+        req.url = url
+        req.method = method
+        req.params = params
+        req.validator = validator
+        req.valid_statuses = valid_statuses
+
+        page.add_request(req)
+        page.requests_groups.append(PageRequestsGroup(req))
+
+        ret = self._browser_navigate(page)
+        return ret
+
+    def http_get(self, url, params=None, validator=None, header=None, valid_statuses=None):
+        return self._http_request("GET", url, params, validator, header, valid_statuses).data
+
+    def http_post(self, url, params=None, validator=None, header=None, valid_statuses=None):
+        return self._http_request("POST", url, params, validator, header, valid_statuses).data
+
+    def http_action(self, method, url, params=None, validator=None, header=None, valid_statuses=None):
+        # Specialized version: http request executor with extended return
+        page = self._http_request(method, url, params, validator, header, valid_statuses)
+        request = page.requests[-1]  # get last request in redirected requests chain
+        return PageWithActions(request.page_actions, page.data, request.url)
+
+    # === printing facilities === #
+
+    def print_browser_info(self):
+        self.print_stats_title("Browser summary")
+        print("  - platform: %s" % os.sys.platform)
+        print("  - browser:  %s" % self.engine)
+        print("  - PID:      %d" % self.pid)
+
+
+##############################################################################
+# Autotests
+##############################################################################
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    b = BrowserPython()
+    b.navigate_to("https://example.com/")
+    b.browser_stop()
