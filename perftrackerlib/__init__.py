431a1c75fef8c40dc939da7a85403e0f00f60391
perfguru87 <perfguru87@gmail.com>
2018-06-29 20:12:20 +0300

    perftrackerlib - added perftrackerlib_require_version()

--- perftrackerlib/__init__.py
+++ perftrackerlib/__init__.py
@@ -1,2 +1,14 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+from distutils.version import LooseVersion
 
 __version__ = "0.0.14"
+
+def perftrackerlib_require_version(ver_required):
+    if LooseVersion(__version__) < LooseVersion(ver_required):
+        print("Error: perftrackerlib version >= %s must be installed, found %s" %
+              (ver_required, __version__))
+        sys.exit(-2)
