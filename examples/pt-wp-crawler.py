fdaeb7e6a1b8d89464d22682a8fd8359d73edca2
perfguru87 <perfguru87@gmail.com>
2018-07-31 23:45:50 +0300

    move WP admin navigation crawler to the browser/wpa_cp_engine.py library

--- examples/pt-wp-crawler.py
+++ examples/pt-wp-crawler.py
@@ -14,78 +14,23 @@ basename = basename.split(".")[0]
 
 from optparse import OptionParser, OptionGroup, IndentedHelpFormatter
 from perftrackerlib.browser.cp_crawler import CPCrawler, CPCrawlerException
-from perftrackerlib.browser.cp_engine import CPEngineBase, CPLoginForm, CPMenuItemXpath
+from perftrackerlib.browser.wpa_cp_engine import CPWPAdminConsole
 from distutils.version import LooseVersion
 
-from perftrackerlib import __version__ as perftrackerlib_version
-PERFTRACKERLIB_VERSION_REQUIRED = '0.0.5'
-if LooseVersion(perftrackerlib_version) < LooseVersion(PERFTRACKERLIB_VERSION_REQUIRED):
-    print("Error: perftrackerlib version >= %s must be installed, found %s" %
-          (PERFTRACKERLIB_VERSION_REQUIRED, perftrackerlib_version))
-    sys.exit(-2)
-
-
-class CPWPAdminConsole(CPEngineBase):
-    type = "WordPress admin console"
-    menu_dom_clicks = False
-    menu_xpaths = [[CPMenuItemXpath(0, None,
-                                    "//a[contains(@class, 'menu-top')]",
-                                    "./div[contains(@class, 'wp-menu-name')]")],
-                   [CPMenuItemXpath(1, None, "//ul[contains(@class, 'wp-submenu')]/li/a", None)]
-                   ]
-
-    login_form = CPLoginForm(sbmt_ids=[('input', 'wp-submit')])
-
-    def _get_page_title(self):
-        divs = self.browser.driver.find_elements_by_xpath("//div[contains(@id, 'wpbody-content')]/div/h1")
-        for div in divs:
-            if not div.is_displayed():
-                continue
-            return div.text
-        return ''
-
-    def cp_get_menu_item_title(self, title_el):
-        text = title_el.get_attribute("innerHTML")
-        return text.split("<")[0]
-
-    def cp_validate_current_page(self, url):
-        curr_url = self.cp_get_current_url()
-
-        if "reauth=1" in curr_url:
-            self.browser.log_error("WARNING: seems like WP control panel session is expired, trying to re-login")
-            time.sleep(2.0)
-            if not self.cp_do_login(url):
-                raise CPCrawlerException("Re-login failed")
-            return False
-
-        return True
-
-    def cp_skip_menu_item(self, link_el, title, link_url):
-        if title in ("Customize", "Header", "Editor"):
-            return True
-        return CPEngineBase.cp_skip_menu_item(self, link_el, title, link_url)
-
-    def cp_menu_item_click(self, el, timeout_s=None, title=None):
-        # BackupConsole doesn't re-render the page, it means default
-        # dom click wait callback will not work and we need own
-
-        text = self._get_page_title()
-
-        def wait_callback(self, el, timeout_s, title):
-            # FIXME: is there a better way to check frame is loaded then to check the title has changed?
-            time.sleep(1.0)
-
-            deadline = time.time() + min(timeout_s, 5.0)
-            while text != self._get_page_title() and time.time() < deadline:
-                time.sleep(1.0)
-
-        self.browser.dom_click(el, timeout_s=timeout_s, name=title, wait_callback=wait_callback, wait_callback_obj=self)
-
+# from perftrackerlib import __version__ as perftrackerlib_version
+# PERFTRACKERLIB_VERSION_REQUIRED = '0.0.5'
+# if LooseVersion(perftrackerlib_version) < LooseVersion(PERFTRACKERLIB_VERSION_REQUIRED):
+#    print("Error: perftrackerlib version >= %s must be installed, found %s" %
+#          (PERFTRACKERLIB_VERSION_REQUIRED, perftrackerlib_version))
+#    sys.exit(-2)
+#
+# see perftrackerlib/browser/wpa_cp_engine.py as reference implementation
+#
 
 def main():
     usage = "usage: %prog [options] URL [URL2 [URL3 ...]]"
 
-    description = "Example: -m -U admin -P pass https://demos1.softaculous.com/WordPress/wp-login.php"
+    description = "Example: -m -U user -P user https://demo.wpjobboard.net/wp-login.php"
 
     workdir = os.path.join(gettempdir(), "%s.%d" % (basename, os.getpid()))
     logfile = os.path.join(workdir, basename + ".log")
@@ -98,7 +43,7 @@ def main():
     opts, args = op.parse_args()
 
     if not args:
-        op.error("URL is not specified")
+        op.error("URL is not specified, %s" % description)
         sys.exit(-1)
 
     cpc.init_opts(opts, args)
