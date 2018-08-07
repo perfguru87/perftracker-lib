436b54da36b40fcdefc4ee58568fb625b54313e6
perfguru87 <perfguru87@gmail.com>
2018-08-07 04:16:21 +0300

    MINOR - update make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 VER=`grep "__version__ = " perftrackerlib/__init__.py | cut -d "\"" -f 2`
 
+echo vim perftracker-lib.spec
 echo "git commit -m \"bump version to $VER\" perftrackerlib/__init__.py && git tag \"v$VER\" && git push origin --tags"
 echo python3 setup.py sdist bdist_wheel
-echo twine upload dist/pertrackerlib*.tgz
+echo twine upload dist/perftrackerlib*.tar.gz
