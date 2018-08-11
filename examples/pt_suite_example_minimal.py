9989909db5cf63c1b567c6c0778d1ab2c2bef378
perfguru87 <perfguru87@gmail.com>
2018-08-11 09:24:33 +0300

    examples/pt_suite_example_minimal.py - new minimal example

---
+++ examples/pt_suite_example_minimal.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser, OptionGroup
+import os
+import sys
+import logging
+import random
+from math import sqrt
+
+bindir, basename = os.path.split(sys.argv[0])
+sys.path.insert(0, os.path.join(bindir, ".."))
+
+from perftrackerlib.client import ptSuite, ptHost, ptVM, ptComponent, ptProduct, ptTest
+from perftrackerlib import __version__
+
+
+def main(suite):
+    suite.addTest(ptTest("Simple benchmark",
+                         description="A simple benchmark output",
+                         metrics="loops/sec", scores=[random.randint(10, 20) / 10.0],
+                         loops=100))
+
+    suite.upload()
+
+
+if __name__ == "__main__":
+
+    op = OptionParser("PerfTracker suite example")
+    op.add_option("-v", "--verbose", action="store_true", help="enable verbose mode")
+
+    suite = ptSuite(suite_ver="1.0.0", product_name="My product", product_ver="1.0-123")
+
+    opts, args = op.parse_args()
+
+    loglevel = logging.DEBUG if opts.verbose else logging.INFO
+    logging.basicConfig(level=loglevel, format="%(asctime)s - %(module)s - %(levelname)s - %(message)s")
+
+    main(suite)
