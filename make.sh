4db2966de7142528d926abee9c5446db6f923b53
perfguru87 <perfguru87@gmail.com>
2018-08-11 09:34:30 +0300

    MINOR - make.sh - fix the 'twine upload' commandline

--- make.sh
+++ make.sh
@@ -5,5 +5,5 @@ echo "\n###### run the following commands manually ######\n"
 echo "vim perftracker-lib.spec # update version"
 echo "git commit -m \"bump version to $VER\" perftrackerlib/__init__.py && git tag \"v$VER\" && git push origin --tags"
 echo python3 setup.py sdist bdist_wheel
-echo twine upload dist/perftrackerlib*.tar.gz
+echo twine upload dist/perftrackerlib-$VER.tar.gz
 echo
