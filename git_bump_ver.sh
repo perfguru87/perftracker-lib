876720f8c04c434c7ac8924e0ab827248f5ac5d2
perfguru87 <perfguru87@gmail.com>
2018-08-11 09:16:16 +0300

    perftrackerlib/client.py - make the addOptions/handleOptions calls optional
    Now it is possible to pass the perftracker server URL to the ptSuite constructor,
    http://127.0.0.1:9000 is the default

--- git_bump_ver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-VER=`grep "__version__ = " perftrackerlib/__init__.py | cut -d "\"" -f 2`
-
-echo git commit -m \"bump version to $VER\"
-echo git tag "v$VER"
-echo git push origin --tags
