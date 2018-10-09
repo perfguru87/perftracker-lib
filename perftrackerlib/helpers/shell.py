a40e2bc2c00a61c0fa1bcdfa86c4aafa20877fbd
perfguru87 <perfguru87@gmail.com>
2018-10-09 12:31:16 +0300

    MINOR - fix cpu frequency calculation on Linux

--- perftrackerlib/helpers/shell.py
+++ perftrackerlib/helpers/shell.py
@@ -96,8 +96,8 @@ class Hw:
             self._vendor = f("cat /sys/class/dmi/id/sys_vendor")
             self._model = f("cat /sys/class/dmi/id/product_name")
             self._cpu_model = f("cat /proc/cpuinfo | grep 'model name' | head -n 1 | cut -d':' -f 2")
-            self._cpu_freq_ghz = 1024 * f("cat /proc/cpuinfo | grep 'cpu MHz' "
-                                          "| head -n 1 | cut -d':' -f 2", float)
+            self._cpu_freq_ghz = round(f("cat /proc/cpuinfo | grep 'cpu MHz' "
+                                          "| head -n 1 | cut -d':' -f 2", float) / 1000, 1)
             self._cpu_count = f("cat /proc/cpuinfo | grep processor | wc -l", int)
             self._ram_kb = f("cat /proc/meminfo | grep MemTotal | awk '{ print $2 }'")
 
