3ce4484a5788e344b84c3ab491e371c129507da6
perfguru87 <perfguru87@gmail.com>
2018-10-09 12:30:50 +0300

    MINOR - test.py - added the helpers/decorators.py testing

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ from execute import execute
 libs = [("perftrackerlib/client.py", 73),
         ("perftrackerlib/helpers/tee.py", 100),
         ("perftrackerlib/helpers/shell.py", 50),
+        ("perftrackerlib/helpers/decorators.py", 75),
         ("perftrackerlib/helpers/timeparser.py", 98),
         ("perftrackerlib/helpers/timeline.py", 89),
         ("perftrackerlib/helpers/largelogfile.py", 98),
