05aa035cc71a44bf6b4e6bec336d70b0aaf831f3
perfguru87 <perfguru87@gmail.com>
2018-10-20 15:41:06 +0300

    bump version to 0.0.38

--- perftrackerlib/__init__.py
+++ perftrackerlib/__init__.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import sys
 from distutils.version import LooseVersion
 
-__version__ = "0.0.37"
+__version__ = "0.0.38"
 __name__ = "perftrackerlib"
 
 def perftrackerlib_require_version(ver_required):
