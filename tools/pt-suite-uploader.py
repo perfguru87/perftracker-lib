0d0829e91c2112369860a32a688f356c687c639a
perfguru87 <perfguru87@gmail.com>
2018-12-02 21:16:15 +0300

    MAJOR - pt-suite-uploader.py - support the perftacker job json format as input and change -j option behaviour: was '-f my.json -j', now '-j my.json'

--- tools/pt-suite-uploader.py
+++ tools/pt-suite-uploader.py
@@ -42,102 +42,126 @@ def _bool(val):
     return val in ("True", "true", True, "Yes", "yes", "y", 1)
 
 
-def run(suite, as_json, from_file, cmdline):
+def parse_test(json_data):
+    if 'tag' not in json_data:
+        return None
 
-    if from_file:
-        f = open(from_file, 'r')
-        out = f.read()
-        f.close()
+    d = json_data
+    try:
+        t = ptTest(d['tag'],
+                   scores=[float(d['score'])] if d.get('score', None) else d['scores'],
+                   deviations=[float(d['deviation'])] if d.get('deviation', None) else d.get('deviations', []),
+                   duration_sec=float(d['duration_sec']) if d.get('duration_sec', None) else 0,
+                   loops=int(d['loops']) if d.get('loops', None) else 1,
+                   metrics=d['metrics'],
+                   cmdline=d.get('cmdline', ''),
+                   less_better=_bool(d.get('less_better', False)),
+                   errors=int(d.get('errors', 0)),
+                   warnings=int(d.get('warnings', 0)),
+                   group=d.get('group', ''),
+                   category=d.get('category', ''),
+                   description=d.get('description', ''))
+    except TypeError as e:
+        logging.error("can't parse: %s" % (str(d)))
+        raise
+
+    return t
+
+
+def validate_test(test, line):
+    if test is None:
+        return False
+
+    if not test.tag:
+        logging.debug("'tag' is not found: %s" % line)
+        return False
+
+    if len(test.scores) == 0:
+        logging.warning("'scores' is not found: %s" % line)
+        return False
+
+    logging.info("parsed: %s" % repr(test))
+    return True
+
+
+def run(cmdline):
+    status, contents, err = execute(cmdline)
+    if status:
+        print("command line failed with status %d" % status)
+        sys.exit(-1)
+
+    contents = contents.strip()
+    if type(contents) == bytes:
+        contents = contents.decode()
+    return contents
+
+
+def read_file(filename):
+    f = open(filename, 'r')
+    content = f.read()
+    f.close()
+
+    content = content.strip()
+    if type(content) == bytes:
+        content = content.decode()
+
+    return content
+
+
+def parse_json(suite, text):
+    data = json.loads(text)
+
+    if 'tests' in data:
+        suite.initFromJson(data)
     else:
-        status, out, err = execute(cmdline)
-        if status:
-            print("command line failed with status %d" % status)
-            sys.exit(-1)
-        out = out
-
-    out = out.strip()
-    if type(out) == bytes:
-        out = out.decode()
-
-    def validate(test, line):
-        if not test.tag:
-            logging.debug("'tag' is not found: %s" % line)
-            return False
-
-        if len(test.scores) == 0:
-            logging.warning("'scores' is not found: %s" % line)
-            return False
-
-        logging.info("parsed: %s" % repr(test))
-        return True
-
-    if as_json:
-        data = json.loads(out)
         for d in data:
-            try:
-                t = ptTest(d['tag'],
-                           scores=[float(d['score'])] if d.get('score', None) else d['scores'],
-                           deviations=[float(d['deviation'])] if d.get('deviation', None) else d.get('deviations', []),
-                           duration_sec=float(d['duration_sec']),
-                           loops=int(d['loops']),
-                           metrics=d['metrics'],
-                           cmdline=d.get('cmdline', ''),
-                           less_better=_bool(d.get('less_better', False)),
-                           errors=int(d.get('errors', 0)),
-                           warnings=int(d.get('warnings', 0)),
-                           group=d.get('group', ''),
-                           category=d.get('category', ''),
-                           description=d.get('description', ''))
-            except TypeError as e:
-                logging.error("can't parse: %s" % (str(d)))
-                raise
-
-            if validate(t, str(d)):
+            t = parse_test(d)
+            if validate_test(t, str(d)):
                 suite.addTest(t)
-    else:
-        r = re.compile("(?P<tag>\w+):\s+(?P<val>.*)$")
 
-        data = []
 
-        for line in out.split("\n"):
-            test = ptTest("")
+def parse_text(suite, text):
+    r = re.compile("(?P<tag>\w+):\s+(?P<val>.*)$")
 
-            for kv in line.split(";"):
-                m = r.match(kv.strip())
-                if not m:
-                    continue
-                tag = m.group('tag').lower()
-                val = m.group('val')
+    data = []
 
-                try:
-                    if tag == 'score':
-                        test.scores = [float(val)]
-                    elif tag == 'deviation':
-                        test.deviations = [float(val)]
-                    elif tag in ('scores', 'deviations'):
-                        test.__dict__[tag] = [float(v) for v in json.loads(val)]
-                    elif tag in ('duration_sec',):
-                        test.__dict__[tag] = float(val)
-                    elif tag in ('loops', 'errors', 'warnings'):
-                        test.__dict__[tag] = int(val)
-                    elif tag in ('tag', 'metrics', 'cmdline', 'group', 'category', 'description'):
-                        test.__dict__[tag] = val
-                    elif tag in ('less_better', ):
-                        test.__dict__[tag] = _bool(val)
-                except ValueError as e:
-                    logging.error("error in line: %s" % line)
-                    logging.error(str(e))
-                    sys.exit(-1)
+    for line in text.split("\n"):
+        test = ptTest("")
 
-            if validate(test, line):
-                suite.addTest(test)
+        for kv in line.split(";"):
+            m = r.match(kv.strip())
+            if not m:
+                continue
+            tag = m.group('tag').lower()
+            val = m.group('val')
+
+            try:
+                if tag == 'score':
+                    test.scores = [float(val)]
+                elif tag == 'deviation':
+                    test.deviations = [float(val)]
+                elif tag in ('scores', 'deviations'):
+                    test.__dict__[tag] = [float(v) for v in json.loads(val)]
+                elif tag in ('duration_sec',):
+                    test.__dict__[tag] = float(val)
+                elif tag in ('loops', 'errors', 'warnings'):
+                    test.__dict__[tag] = int(val)
+                elif tag in ('tag', 'metrics', 'cmdline', 'group', 'category', 'description'):
+                    test.__dict__[tag] = val
+                elif tag in ('less_better', ):
+                    test.__dict__[tag] = _bool(val)
+            except ValueError as e:
+                logging.error("error in line: %s" % line)
+                logging.error(str(e))
+                sys.exit(-1)
+
+        if validate_test(test, line):
+            suite.addTest(test)
 
     logging.info("%d tests found" % len(suite.tests))
     if not len(suite.tests):
         print("aborting")
         sys.exit(-1)
-
-    suite.upload()
 
 
 class formatter(IndentedHelpFormatter):
@@ -199,8 +223,8 @@ def main():
 
     op = OptionParser(description=description, usage=usage, formatter=formatter())
     op.add_option("-v", "--verbose", action="store_true", help="enable verbose mode")
-    op.add_option("-j", "--json", action="store_true", help="treat input data as json")
-    op.add_option("-f", "--file", help="get results from file, not from command line")
+    op.add_option("-j", "--json", help="get results from json file, not from command line")
+    op.add_option("-f", "--file", help="get results from text file, not from command line")
 
     suite = ptSuite()
     suite.addOptions(op)
@@ -210,14 +234,26 @@ def main():
     loglevel = logging.DEBUG if opts.verbose else logging.INFO
     logging.basicConfig(level=loglevel, format="%(asctime)s - %(module)17s - %(levelname).3s - %(message)s", datefmt='%H:%M:%S')
 
-    suite.handleOptions(opts)
+    if opts.json:
+        parse_json(suite, read_file(opts.json))
+        suite.handleOptions(opts)
+    elif opts.file:
+        parse_text(suite, read_file(opts.file))
+        suite.handleOptions(opts)
+    else:
+        suite.handleOptions(opts)
+        if len(args) == 0:
+            op.print_usage()
+            print("error: command line is required")
+            sys.exit(-1)
+        parse_text(suite, run(suite, subprocess.list2cmdline(args)))
 
-    if not opts.file and len(args) == 0:
-        op.print_usage()
-        print("error: command line is required")
+    logging.info("%d tests found" % len(suite.tests))
+    if not len(suite.tests):
+        print("aborting")
         sys.exit(-1)
 
-    run(suite, opts.json, opts.file, subprocess.list2cmdline(args))
+    suite.upload()
 
 
 if __name__ == "__main__":
