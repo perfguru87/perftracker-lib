478b7aa49d1e524343bf932cea56c0d00b5765da
perfguru87 <perfguru87@gmail.com>
2018-12-11 01:40:12 +0300

    MINOR - update examples/pt_suite_example_fake_microservice.py

--- examples/pt_suite_example_fake_microservice.py
+++ examples/pt_suite_example_fake_microservice.py
@@ -26,68 +26,68 @@ def main(suite):
         group = "Provisioning tests"
 
         suite.addTest(ptTest("Create accounts in 1 thread",
-                             group=group, category=volume, metrics="accounts/sec",
+                             group=group, category=volume, metrics="accounts/sec", duration_sec=900,
                              description="POST /accounts in 1 thread (create %d accounts)" % (chunk * 0.1),
                              scores=[40 / sqrt(sqrt(accounts_in_db + 100))]))  # fake score
 
         suite.addTest(ptTest("Create accounts in 16 threads",
-                             group=group, category=volume, metrics="accounts/sec",
+                             group=group, category=volume, metrics="accounts/sec", duration_sec=3200,
                              description="POST /accounts in 16 threads (create %d accounts)" % (chunk * 0.9),
                              scores=[500 / (2 * sqrt(accounts_in_db + 100))]))  # fake score
 
         group = "Read-only tests"
 
         suite.addTest(ptTest("GET information for some account in 1 thread",
-                             group=group, category=volume, metrics="accounts/sec",
+                             group=group, category=volume, metrics="accounts/sec", duration_sec=30,
                              description="GET /account/1 in 1 thread (30 sec)",
                              scores=[110 - sqrt(accounts_in_db + 10)]))  # fake score
 
         suite.addTest(ptTest("GET information for some account in 8 threads",
-                             group=group, category=volume, metrics="accounts/sec",
+                             group=group, category=volume, metrics="accounts/sec", duration_sec=30,
                              description="GET /account/1 in 8 threads (30 sec)",
                              scores=[420 - 2 * sqrt(accounts_in_db + 10)]))  # fake score
 
         suite.addTest(ptTest("GET information for some account in 64 threads",
-                             group=group, category=volume, metrics="accounts/sec",
+                             group=group, category=volume, metrics="accounts/sec", duration_sec=30,
                              description="GET /account/1 in 64 threads (30 sec)",
                              scores=[750 - 2.5 * sqrt(accounts_in_db + 10)]))  # fake score
 
         suite.addTest(ptTest("GET list of 10 first accounts in 1 thread",
-                             group=group, category=volume, metrics="lists/sec",
+                             group=group, category=volume, metrics="lists/sec", duration_sec=30,
                              description="GET /accounts/?limit=10 in 1 thread (30 sec)",
                              scores=[85 - 0.4 * sqrt(accounts_in_db + 10)]))  # fake score
 
         suite.addTest(ptTest("GET list of 10 first accounts in 8 thread",
-                             group=group, category=volume, metrics="lists/sec",
+                             group=group, category=volume, metrics="lists/sec", duration_sec=30,
                              description="GET /accounts/?limit=10 in 1 thread (30 sec)",
                              scores=[95 - 0.5 * sqrt(accounts_in_db + 10)]))  # fake score
 
         suite.addTest(ptTest("GET list of 10 first accounts in 64 thread",
-                             group=group, category=volume, metrics="lists/sec",
+                             group=group, category=volume, metrics="lists/sec", duration_sec=30,
                              description="GET /accounts/?limit=10 in 64 thread (30 sec)",
                              scores=[105 - 0.6 * sqrt(accounts_in_db + 10)]))  # fake score
 
         group = "Modificating tests"
 
         suite.addTest(ptTest("Update accounts in 1 thread",
-                             group=group, category=volume, metrics="accounts/sec",
+                             group=group, category=volume, metrics="accounts/sec", duration_sec=89,
                              description="PUT /accounts in 1 thread (update 100 accounts)",
                              scores=[100 - 0.3 * sqrt(accounts_in_db + 10)]))  # fake score
 
         suite.addTest(ptTest("Update accounts in 16 threads",
-                             group=group, category=volume, metrics="accounts/sec",
+                             group=group, category=volume, metrics="accounts/sec", duration_sec=970,
                              description="PUT /accounts in 16 threads (update 1600 accounts)",
                              scores=[100 - 0.4 * sqrt(accounts_in_db + 10)]))  # fake score
 
         suite.addTest(ptTest("Delete accounts in 1 thread",
-                             group=group, category=volume, metrics="accounts/sec",
+                             group=group, category=volume, metrics="accounts/sec", duration_sec=520,
                              description="PUT /accounts in 1 thread (update 100 accounts)",
-                             scores=[100 - 0.3 * sqrt(accounts_in_db + 10)]))  # fake score
+                             scores=[10 - 0.3 * sqrt(accounts_in_db + 10)]))  # fake score
 
         suite.addTest(ptTest("Delete accounts in 16 threads",
-                             group=group, category=volume, metrics="accounts/sec",
+                             group=group, category=volume, metrics="accounts/sec", duration_sec=540,
                              description="PUT /accounts in 16 threads (update 1600 accounts)",
-                             scores=[1600 - 0.3 * sqrt(accounts_in_db + 10)]))  # fake score
+                             scores=[160 - 0.3 * sqrt(accounts_in_db + 10)]))  # fake score
 
         suite.upload()
 
